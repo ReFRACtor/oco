9ab5ade5698c3d5ddc3f23ac1a6d4025bc513629
James McDuffie <James.McDuffie@jpl.nasa.gov>
2019-07-31 10:18:52 -0700

    Separate configuration into a base configuration with seperate files for the retrieval and simulation configs.
    * Fix some issues in create_simulation_file.py.
    * Rename simulation.py to simulation_file.py to make it distinct from
    simulation_config.py

---
+++ config/retrieval_config.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+import refractor.factory.creator as creator
+from refractor import framework as rf
+
+from oco import Level1bOco, OcoMetFile, OcoSoundingId, OcoNoiseModel
+
+from base_config import base_config_definition
+
+def oco_level1b(hdf_obj, observation_id):
+    max_ms = np.array([ 7.00e20, 2.45e20, 1.25e20 ])
+
+    l1b = Level1bOco(hdf_obj, observation_id)
+
+    noise = OcoNoiseModel(hdf_obj, observation_id, max_ms)
+    l1b.noise_model = noise
+
+    return l1b
+
+def oco_meteorology(met_file, observation_id):
+    return OcoMetFile(met_file, observation_id)
+
+def oco_bad_sample_mask(hdf_obj, observation_id):
+    sounding_idx = observation_id.sounding_number 
+
+    # Try to get bad sample list from a dedicated dataset
+    if hdf_obj.has_object("/InstrumentHeader/bad_sample_list"):
+        return hdf_obj.read_double_3d("/InstrumentHeader/bad_sample_list")[:, sounding_idx, :]
+    else:
+        # Else try and get bad sample list from snr_coef dataset for older versions of the OCO-2 product
+        return hdf_obj.read_double_4d("/InstrumentHeader/snr_coef")[:, sounding_idx, :, 2]
+
+# Return ILS information for the observation sounding position
+def ils_delta_lambda(hdf_obj, observation_id):
+    sounding_idx = observation_id.sounding_number 
+    return hdf_obj.read_double_4d("/InstrumentHeader/ils_delta_lambda")[:, sounding_idx, :, :]
+
+def ils_response(hdf_obj, observation_id):
+    sounding_idx = observation_id.sounding_number 
+    return hdf_obj.read_double_4d("/InstrumentHeader/ils_relative_response")[:, sounding_idx, :, :]
+
+def retrieval_config_definition(l1b_file, met_file, sounding_id, **kwargs):
+    l1b_obj = rf.HdfFile(l1b_file)
+    observation_id = OcoSoundingId(l1b_obj, sounding_id)
+
+    config_def = base_config_definition(**kwargs)
+
+    config_def['input'] = {
+        'creator': creator.base.SaveToCommon,
+        'l1b': oco_level1b(l1b_obj, observation_id),
+        'met': oco_meteorology(met_file, observation_id),
+        'sounding_number': observation_id.sounding_number,
+    }
+
+    config_def['scenario'] = {
+        'creator': creator.scenario.ScenarioFromL1b,
+    }
+
+    config_def['spec_win']['bad_sample_mask'] = oco_bad_sample_mask(l1b_obj, observation_id)
+
+    # Instrument values
+    config_def['instrument']['ils_function']['delta_lambda'] = ils_delta_lambda(l1b_obj, observation_id)
+    config_def['instrument']['ils_function']['response'] = ils_response(l1b_obj, observation_id)
+
+    # Ground albedo from radiance continuum level
+    config_def['atmosphere']['ground']['lambertian']['value'] = {
+        'creator': creator.ground.AlbedoFromSignalLevel,
+        'signal_level': {
+            'creator': creator.l1b.ValueFromLevel1b,
+            'field': "signal",
+        },
+        'solar_strength': np.array([4.87e21, 2.096e21, 1.15e21]),
+        'solar_distance': { 'creator': creator.l1b.SolarDistanceFromL1b },
+    } 
+ 
+    return config_def
---
+++ config/simulation_config.py
@@ -0,0 +1,86 @@
+import re
+
+import numpy as np
+
+import refractor.factory.creator as creator
+from refractor import framework as rf
+
+from base_config import base_config_definition, aerosol_prop_file
+
+from simulation_file import SimulationFile
+
+def simulation_config_definition(sim_file, sim_index, **kwargs):
+
+    config_def = base_config_definition(**kwargs)
+
+    # Load simulation file
+    sim_data = SimulationFile(sim_file, sim_index)
+
+    # Load scenario values
+    for val_name in config_def['scenario'].keys():
+        if val_name != "creator":
+            config_def['scenario'][val_name] = getattr(sim_data.scenario, val_name)
+            print(val_name, getattr(sim_data.scenario, val_name))
+
+    config_def['scenario']['sounding_number'] = int(str(sim_data.scenario.observation_id[...])[-1])
+
+    # Instrument values
+    config_def['instrument']['ils_function']['delta_lambda'] = sim_data.instrument.ils_delta_lambda
+    config_def['instrument']['ils_function']['response'] = sim_data.instrument.ils_response
+
+    # Atmosphere
+    config_def['atmosphere']['pressure'] = {
+        'creator': creator.atmosphere.PressureGrid,
+        'pressure_levels': sim_data.atmosphere.pressure_levels,
+        'value': sim_data.atmosphere.surface_pressure,
+    }
+
+    config_def['atmosphere']['temperature'] = {
+        'creator': creator.atmosphere.TemperatureLevelOffset,
+        'temperature_levels': sim_data.atmosphere.temperature,
+        'value': np.array([0.0]),
+    }
+
+    # Absorber values
+    config_def['atmosphere']['gases'] = sim_data.absorber.molecule_names
+
+    for name in sim_data.absorber.molecule_names:
+        config_def['atmosphere']['absorber'][name]["vmr"] = {
+            'creator': creator.absorber.AbsorberVmrLevel,
+            'value': sim_data.absorber.vmr(name),
+        }
+
+    # Aerosol values, reset to start with an empty aerosol configuration
+    config_def['atmosphere']['aerosol'] = {
+        'creator': creator.aerosol.AerosolOptical,
+        'aerosols': sim_data.aerosol.particle_names,
+    }
+
+    for name in sim_data.aerosol.particle_names:
+        config_def['atmosphere']['aerosol'][name] = {
+            'creator': creator.aerosol.AerosolDefinition,
+            'extinction': {
+                'creator': creator.aerosol.AerosolShapeGaussian,
+                'value': sim_data.aerosol.gaussian_param(name),
+            },
+            'properties': {
+                'creator': creator.aerosol.AerosolPropertyHdf,
+                'filename': aerosol_prop_file,
+                'prop_name': sim_data.aerosol.property_name(name),
+            },
+        }
+
+    # Ground lambertian
+    config_def['atmosphere']['ground']['lambertian']['value'] = sim_data.ground.lambertian_albedo
+
+    # Modify EOFs to not scale by the uncertainty since this requires uncertainty from the L1B file if enabled
+    ic_config = config_def['instrument']['instrument_correction']
+    for ic_name in ic_config['corrections']:
+        if re.search('eof_', ic_name):
+            ic_config[ic_name]['scale_uncertainty'] = False
+ 
+    # Require only the state vector to be set up, this gives us jacobians 
+    # without worrying about satisfying solver requirements
+    config_def['retrieval']['creator'] = creator.retrieval.RetrievalBaseCreator
+
+    return config_def
